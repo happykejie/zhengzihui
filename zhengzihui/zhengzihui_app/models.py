b19f8fc43c624fa7a7dda4fe12d4ec381d373f14
shadowkilleri <542440582@qq.com>
2016-04-16 13:23:05 +0800

    merge the code from xcz by mannual from yz

--- zhengzihui/zhengzihui_app/models.py
+++ zhengzihui/zhengzihui_app/models.py
@@ -175,6 +175,59 @@ class tb_News(models.Model):
     )
     new_is_display =  models.CharField(max_length=20,choices=NEWS_IS_DISPLAY_CHOICES,default=NOTDISPLAY,null=False,blank=False,help_text="是否为前端展示新闻")
 	
+
+class Tb_Notice(models.Model):
+    Notice_id = models.AutoField(primary_key = True)  
+    Notice_title = models.CharField('公告标题',max_length=100,null =False)
+    Article_id = models.IntegerField('文章ID',null =False)
+    Notice_time = models.DateField('发布时间')
+    Notice_source = models.CharField('公告来源',max_length=100,null =False)
+    Nocl_id = models.IntegerField('分类ID',null =False)
+    Notice_sort = models.IntegerField('排序',null =False)
+    Notice_is_display= models.IntegerField('是否显示',null =False)
+    Notice_top = models.IntegerField('强制置顶',null =False)
+	
+    def __str__(self):
+        return self.Notice_title
+    def __str__(self):
+        return self.Notice_source
+
+
+class Tb_Notice_Class(models.Model):
+    Nocl_id = models.IntegerField('分类ID',null =False,primary_key = True)
+    Nocl_code = models.IntegerField('分类标识码',null =False)
+    Nocl_name = models.CharField('分类名称',max_length=100,null =False)
+    Nocl_des = models.CharField('分类描述',max_length=100,null =False)
+    Nocl_parent_id = models.IntegerField('父类ID')
+    Notice_sort = models.IntegerField('排序',null =False)
+	
+    def __str__(self):
+        return self.Nocl_name
+    def __str__(self):
+        return self.Nocl_des
+
+
+class Tb_Apage(models.Model):
+     Apage_id = models.AutoField('单页ID',null =False,primary_key = True)
+     Article_id = models.IntegerField('文章ID',null =False)
+     Has_album = models.IntegerField('是否含有相册',null =False)
+     Apage_time = models.DateField('发布时间')
+     Apage_source = models.CharField('单页来源',max_length=100)
+     Apcl_id = models.IntegerField('分类ID',null =False)
+     Apage_sort = models.IntegerField('排序',null =False)
+     Apage_is_display = models.IntegerField('是否显示',null =False)
+     def __str__(self):
+        return self.Apage_source
+    
+class Tb_Apage_Class(models.Model):
+     Apcl_id = models.IntegerField('单页分类ID',null =False,primary_key = True)
+     Apcl_code = models.IntegerField('分类标识码',null =False)
+     Apcl_name = models.CharField('分类名称',max_length=100,null =False)
+     Apcl_parent_id = models.IntegerField('父类ID')
+     Apcl_sort = models.IntegerField('排序',null =False)
+     def __str__(self):
+        return self.Apcl_name
+    
     
     
     
