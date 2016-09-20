6965d0b83034d4c68e90651be0cf31f46297e9d3
shadowkilleri <542440582@qq.com>
2016-09-20 17:43:13 +0800

    基基本完成推送管理

--- zhengzihui/zhengzihui_app/yz_views.py
+++ zhengzihui/zhengzihui_app/yz_views.py
@@ -1,6 +1,8 @@
 #coding=utf-8
 
 from views import *
+from django.http import JsonResponse
+#from django.utils import simplejson
 
 '''
 用户中心
@@ -1356,23 +1358,33 @@ def get_push_info(user_prefer):
     if all_item is None:
         return None
     return all_item[0]
-
-
-def info_push(request):
+def get_all_table_info():
     all_user = tb_user.objects.all()
-    #获得有填写相关领域的用户，即有企业信息的用户
+    # 获得有填写相关领域的用户，即有企业信息的用户
     for user in all_user:
-        if user.expand_id is  None:
+        if user.expand_id is None:
             all_user.remove(user)
 
     all_list_info = []
     for user in all_user:
-        user_prefer  = user.expand.company_industry
+        user_prefer = user.expand.company_industry
         item_push = get_push_info(user_prefer)
-        #第三个字段用户判断是否是已经审核过的推送消息
-        info_list = [user,item_push,0]
+        # 第三个字段用户判断是否是已经审核过的推送消息
+        info_list = [user, item_push, 0]
         all_list_info.append(info_list)
-    all_list_info_show = all_list_info[0:5]
+    all_list_info_show = []
+    if len(all_list_info)>=5:
+        all_list_info_show = all_list_info[0:5]
+    else:
+        all_list_info_show = all_list_info
+    for info_show in all_list_info_show:
+        if len(push_info.objects.filter(push_to_user=info_show[0].user_id, push_item_id=info_show[1].item_id)):
+            info_show[2] = 1
+    return all_list_info_show
+
+def info_push(request):
+
+    all_list_info_show = get_all_table_info()
     return render(request,'info_push.html',{'all_list_info_show':all_list_info_show})
 
 
@@ -1408,5 +1420,180 @@ def shaixuan_push_info(request):
     province = request.GET['province']
     city = request.GET['city']
     distr = request.GET['distr']
+    print province,city,distr
+    all_show_Temp = get_all_table_info()
+    all_show = get_all_table_info()
+    all_show1 = get_all_table_info()
+    all_show2 = get_all_table_info()
 
-    get_fit_item = tb_item.objects.filter(province__contains=province,city__contains=city,distr__contains=distr)
+    for show in all_show_Temp:
+
+        if show[1].privince == province and show[1].city == city and show[1].distr == distr:
+
+            pass
+        else:
+            all_show.remove(show)
+
+
+    if len(all_show) == 0:
+
+        for show in all_show_Temp:
+
+
+            if show[1].privince == str(province) and show[1].city == str(city):
+
+                pass
+            else:
+
+
+                all_show1.remove(show)
+
+    if len(all_show1)==0:
+
+        for show in all_show_Temp:
+
+            if show[1].privince == province :
+                pass
+            else:
+                all_show2.remove(show)
+
+
+    final_all_show = []
+    if len(all_show)==0:
+        if len(all_show1) == 0:
+            if len(all_show2) ==0:
+                return HttpResponse('没有该地区的相关信息')
+            else:
+                pass
+        else:
+            final_all_show = all_show1
+    else:
+        final_all_show = all_show
+    all_list_info_show = final_all_show
+
+    return render(request, 'info_push.html', {'all_list_info_show': all_list_info_show})
+
+'''
+    show_info = []
+    for show in final_all_show:
+        temp = [show[1].item_code,show[1].item_name,show[0].user_name,show[2] ]
+        show_info.append(temp)
+
+    print show_info
+    return JsonResponse(show_info, safe=False)
+'''
+def cmap_yz(request):
+    #ce shi shu ju mu qian xie si le
+
+    #request.COOKIES['user_id']="2016"
+    if 'user_id' in request.GET:
+        user_id = request.GET['user_id']
+        if(len(tb_customcompany.objects.filter(company_id=user_id))==0 ):
+            return  HttpResponse('用戶還未完善申報地圖')
+        else:
+            if(tb_customcompany.objects.get(company_id=user_id)).conclusion=="":
+                return HttpResponse("等待处理")
+            else:
+
+                #conc=tb_customcompany.objects.get(company_id=user_id).conclusion
+                #wu shu ju  xie si le de ce shi shui chan
+                conc="水产"
+                A=tb_user.objects.get(user_id = user_id).user_name
+                B=tb_item.objects.filter(item_about__contains =  conc) .filter(item_level=  1)
+                C=tb_item.objects.filter(item_about__contains =  conc) .filter(item_level=  2)
+                D=tb_item.objects.filter(item_about__contains =  conc) .filter(item_level=  3)
+                E=tb_item.objects.filter(item_about__contains =  conc) .filter(item_level=  4)
+                if (len(B)==0):
+                    B=""
+                else:
+                    B=str(tb_item.objects.filter(item_about__contains =  conc) .filter(item_level=  1)[0].item_id)+"."+tb_item.objects.filter(item_about__contains =  conc) .filter(item_level=  1)[0].item_name
+                if (len(C)==0):
+                    C=""
+                else:
+                    C=str(tb_item.objects.filter(item_about__contains =  conc) .filter(item_level=  2)[0].item_id)+"."+tb_item.objects.filter(item_about__contains =  conc) .filter(item_level=  2)[0].item_name
+                if (len(D)==0):
+                    D=""
+                else:
+                    D=str(tb_item.objects.filter(item_about__contains =  conc) .filter(item_level=  3)[0].item_id)+"."+tb_item.objects.filter(item_about__contains =  conc) .filter(item_level=  3)[0].item_name
+                if (len(E)==0):
+                    E=""
+                else:
+                    E=str(tb_item.objects.filter(item_about__contains =  conc) .filter(item_level=  4)[0].item_id)+"."+tb_item.objects.filter(item_about__contains =  conc) .filter(item_level=  4)[0].item_name
+
+                #A="100"
+                #B="110"
+                #C="120"
+                #D="130"
+                #E="140"
+                return render_to_response("yz_templates/cust_map_yz.html",{'A':A,'B':B,'C':C,'D':D,'E':E})
+    else:
+        return HttpResponse('用戶還未完善申報地圖')
+
+def project_detail_short(request):
+        project_detail_item_id = 1  # 取一个默认值
+        if request.GET['id']:
+            # 能保证取到吗
+            project_detail_item_id = request.GET['id']
+
+        addclick = tb_item_click.objects.get(item_id=project_detail_item_id)
+        if addclick == None:
+            addclick = tb_item_click(itcl_id=0, item_id=project_detail_item_id, click_counter=1)
+            addclick.save()
+        else:
+            addclick.click_counter += 1
+            addclick.save()
+        item = tb_item.objects.get(item_id=project_detail_item_id)
+        item.item_pa_name = (tb_item_pa.objects.get(ipa_id=item.item_pa_id)).ipa_name  # 扩展对象属性，直接填写即可YZ
+        item.item_pa_address = (tb_item_pa.objects.get(ipa_id=item.item_pa_id)).ipa_address
+        # print item.item_pa_name
+        # print item.item_pa_name
+        article = tb_article.objects.filter(affiliation_id=project_detail_item_id)
+
+        a_pics = []
+        # article2 = article[2]
+        if (len(article) == 0):
+            # 获取项目起止时间
+            gettimeInstance = tb_item.objects.get(item_id=project_detail_item_id)
+            # 获得热门推荐的项目
+            recommendtemp = get_the_hotrecommend()
+            recommend = get_and_set_info(recommendtemp)
+
+            album = tb_album.objects.filter(album_type=0, affiliation_id=project_detail_item_id, is_default=1).order_by(
+                '-nacl_sort')[0]  # 获取项目对应的相册id
+            album_id = album.album_id
+            pics = tb_pic.objects.filter(album_id=album_id).order_by('-pic_id')[0:4]  # 获取前四张图片
+            for pic in pics:
+                a_pic = pic.pic_object.url[14:]
+                # print a_pic
+                a_pics.append(a_pic)
+            if not a_pics:
+                pic_url = '/static/zhengzihui_app/img_for_items/default.jpg'
+                a_pics.append(pic_url)
+
+            context = {'item': item, 'article': article, 'a_pics': a_pics, 'recommend': recommend,
+                       'gettimeInstance': gettimeInstance,}
+            # print item.item_pa_address
+
+            return render(request, 'yz_templates/project_detail_short.html', context)
+
+        else:
+
+            album = tb_album.objects.filter(album_type=0, affiliation_id=project_detail_item_id, is_default=1).order_by(
+                '-nacl_sort')[0]  # 获取项目对应的相册id
+            album_id = album.album_id
+            pics = tb_pic.objects.filter(album_id=album_id).order_by('-pic_id')[0:4]  # 获取前四张图片
+            for pic in pics:
+                a_pic = pic.pic_object.url[14:]
+                # print a_pic
+                a_pics.append(a_pic)
+
+            # 获取项目起止时间
+            gettimeInstance = tb_item.objects.get(item_id=project_detail_item_id)
+
+            # 获得热门推荐的项目
+            recommendtemp = get_the_hotrecommend()
+            recommend = get_and_set_info(recommendtemp)
+            print "sdklfsdlflsdkflsdklksdlfksdlfksdkl"
+            context = {'item': item, 'article': article[0], 'a_pics': a_pics, 'recommend': recommend,
+                       'gettimeInstance': gettimeInstance,}
+            return render(request, 'yz_templates/project_detail_short.html', context)
