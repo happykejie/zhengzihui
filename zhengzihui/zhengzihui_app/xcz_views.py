6bc226f046d697c73ec62809bee7affa044c8f69
Xucz <15208171819@163.com>
2016-10-09 09:43:41 +0800

    index search bug ok

--- zhengzihui/zhengzihui_app/xcz_views.py
+++ zhengzihui/zhengzihui_app/xcz_views.py
@@ -58,7 +58,7 @@ def Searchgoods(request):
         search_content = "全部"
         if goodsname is not None:
 	#fenleisousuo	
-            if  ((typefsearch.encode("utf-8") )=="发布部门"):
+            if  ((typefsearch.encode("utf-8") )=="所属行业"):
                 selected['bumen'] = goodsname.encode("utf-8")
                 request.session['bumen'] = goodsname.encode("utf-8")
                 request.session['search_content'] = goodsname.encode("utf_8")
@@ -151,7 +151,7 @@ def search_result(request):
     else:
         selected['zhuangtai'] = ''
 	
-
+    #print(selected['bumen'])
     allthebumen = ['经济与信息化','发展与改革','财政','科技','教育','文化','卫计','体育','知识产权','农业','林业','畜牧','渔业','粮食','中医药','国土','住建','交通','水利','能源','环保','商务','投资促进','工商','税务','民政','人社','扶贫','旅游','人民银行','银监','证监','保监','质监','药监','安监']
     allthejibie = ['县级财政资金','市级财政资金','省级财政资金','中央财政资金']
     allthezhuangtai = ['截止申报','正在申报']
@@ -165,6 +165,7 @@ def search_result(request):
         middle_items = list(middle_items)
     else:
     	middle_items=tb_item.objects.all()
+    #print(middle_items)
     
     if  (selected['zhuangtai'].encode("utf-8") != '全部'):
 
@@ -174,26 +175,33 @@ def search_result(request):
     			tmiddle_items.append(i)
     else:
     	tmiddle_items=middle_items
-
+    #print(tmiddle_items)
     list_temp2 = []
     if  (selected['bumen'].encode("utf-8") != '全部'):
         bumenlist = (selected['bumen'].encode("utf-8")).split(',')
         for bumen in bumenlist:
-            list_temp1 = bumen.split(":",1)
-            if len(list_temp1)>1:
-                str_temp = list_temp1[1]
-                list_temp2 = str_temp.split("/")
+	    #print (bumen)
+	    if ":" not in bumen:
+		#print (bumen)
+		for i in tmiddle_items:
+			if bumen in (i.item_about).encode("utf-8"):
+				items.append(i)
+	    else:
+            	list_temp1 = bumen.split(":",1)
+            	if len(list_temp1)>1:
+                	str_temp = list_temp1[1]
+                	list_temp2 = str_temp.split("/")
 
-            if len(list_temp2)!=0:
-                for i in tmiddle_items:
-                    for j in list_temp2:
-                        if j in (i.item_about).encode("utf-8"):
-                            if i not in items:#去重复
-                                items.append(i)
+           	if len(list_temp2)!=0:
+                	for i in tmiddle_items:
+                    		for j in list_temp2:
+                        		if j in (i.item_about).encode("utf-8"):
+                            			if i not in items:#去重复
+                                			items.append(i)
+	    
     else:
-
     	items=tmiddle_items
-
+    #print(items)
     if (len(items)>10):
     	items = items[:10]#不够10条报错
     else:
